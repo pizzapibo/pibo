8cfe6809bbd76a2caf0cceef3ea745384489a677
pizzapibo <pizzapibo@gmail.com>
2026-08-05 12:32:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -650,7 +650,27 @@ console.log(
 "Welcome."
 
 );
+if("serviceWorker" in navigator){
 
+window.addEventListener("load",()=>{
+
+navigator.serviceWorker.register("sw.js")
+
+.then(()=>{
+
+console.log("Service Worker Ready");
+
+})
+
+.catch(err=>{
+
+console.log(err);
+
+});
+
+});
+
+}
 // ===========================================
 // END
 // ===========================================
